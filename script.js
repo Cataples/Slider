460c6fd48dfa6c2c6f01de1ada0dbe0a2c0d983e
Catalin <catalin.ples@gmail.com>
2018-05-17 14:48:29 +0300

    finished slider

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
+let container = document.getElementsByClassName("viewport")[0]; 
+let rightBtn = document.getElementsByClassName("rightButton")[0];
+let leftBtn = document.getElementsByClassName("leftButton")[0];
+let imgsContainer = document.getElementsByClassName("image-container")[0];
+imgsContainer.style.left = "0px";
+imgsContainer.style.transition = "all 1s";
+
+rightBtn.addEventListener("click", function(){
+    let oldLeft = imgsContainer.style.left.split("px")[0];
+
+    if(oldLeft == -3600) {
+        imgsContainer.style.left = "0px"
+    } else {
+        imgsContainer.style.left = (parseInt(oldLeft) - 900) + "px";
+    }
+})
+
+leftBtn.addEventListener("click", function(){
+    let oldLeft = imgsContainer.style.left.split("px")[0];
+
+    if(oldLeft == 0) {
+        imgsContainer.style.transition = "all 1s";
+        imgsContainer.style.left = "-3600px"
+    } else {
+        imgsContainer.style.left = (parseInt(oldLeft) + 900) + "px";
+    }
+})
 
 
